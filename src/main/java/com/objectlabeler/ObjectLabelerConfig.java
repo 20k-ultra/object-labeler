58a2413b7c5b44dd0d7cafc826af76adb186c49a
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2022-06-18 23:42:16 -0400

    Setup keybind to start taking screenshots
    Signed-off-by: 20k-ultra <3946250+20k-ultra@users.noreply.github.com>

--- src/main/java/com/objectlabeler/ObjectLabelerConfig.java
+++ src/main/java/com/objectlabeler/ObjectLabelerConfig.java
@@ -1,19 +1,35 @@
 package com.objectlabeler;
 
+import java.awt.event.KeyEvent;
+
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Keybind;
 
 @ConfigGroup("objectlabeler")
 public interface ObjectLabelerConfig extends Config
 {
 	@ConfigItem(
-		keyName = "targetObject",
-		name = "Target",
-		description = "Object you want to label"
+			keyName = "targetObject",
+			name = "Target",
+			description = "Object you want to label",
+			position = 0
 	)
 	default String targetObject()
 	{
 		return "";
 	}
+
+	@ConfigItem(
+			keyName = "hotkey",
+			name = "Record hotkey",
+			description = "When you press this key the plugin will start to label objects",
+			position = 1
+	)
+	default Keybind hotkey()
+	{
+		return new Keybind(KeyEvent.CTRL_DOWN_MASK, 1);
+	}
+
 }
--- src/main/java/com/objectlabeler/ObjectLabelerPlugin.java
+++ src/main/java/com/objectlabeler/ObjectLabelerPlugin.java
@@ -12,28 +12,76 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+import java.util.function.Consumer;
+import java.util.concurrent.ScheduledExecutorService;
+
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.events.ConfigChanged;
+import net.runelite.client.input.KeyManager;
+import static net.runelite.client.RuneLite.SCREENSHOT_DIR;
+import net.runelite.client.util.HotkeyListener;
+import net.runelite.api.events.GameTick;
+import net.runelite.client.util.ImageCapture;
+import net.runelite.client.util.ImageUploadStyle;
+import net.runelite.client.ui.DrawManager;
+
 @Slf4j
 @PluginDescriptor(
 	name = "Object Labeler"
 )
 public class ObjectLabelerPlugin extends Plugin
 {
+
+	private boolean labelingActive = false;
+	private int ticks = 0;
+	private static final int tickTrigger = 1;
+
 	@Inject
 	private Client client;
 
 	@Inject
 	private ObjectLabelerConfig config;
 
+	@Inject
+	private ClientThread clientThread;
+
+	@Inject
+	private KeyManager keyManager;
+
+	@Inject
+	private ImageCapture imageCapture;
+
+	@Inject
+	private ScheduledExecutorService executor;
+
+	@Inject
+	private DrawManager drawManager;
+
+	private final HotkeyListener hotkeyListener = new HotkeyListener(() -> config.hotkey())
+	{
+		@Override
+		public void hotkeyPressed()
+		{
+			toggleLabeling();
+		}
+	};
+
 	@Override
 	protected void startUp() throws Exception
 	{
 		log.info("Object Labeler started!");
+		SCREENSHOT_DIR.mkdirs();
+		keyManager.registerKeyListener(hotkeyListener);
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
 		log.info("Object Labeler stopped!");
+		keyManager.unregisterKeyListener(hotkeyListener);
 	}
 
 	@Subscribe
@@ -50,5 +98,85 @@ public class ObjectLabelerPlugin extends Plugin
 	ObjectLabelerConfig provideConfig(ConfigManager configManager)
 	{
 		return configManager.getConfig(ObjectLabelerConfig.class);
+	}
+
+	@Subscribe
+	public void onConfigChanged(ConfigChanged configChanged)
+	{
+		if (!configChanged.getGroup().equals("objectlabeler"))
+		{
+			return;
+		}
+
+		clientThread.invoke(this::rebuild);
+	}
+
+	@Subscribe
+	public void onGameTick(GameTick event)
+	{
+		this.ticks++;
+
+		if (this.ticks > tickTrigger) {
+			this.ticks = 0;
+
+			if (this.labelingActive && client.getGameState() != GameState.LOGIN_SCREEN)
+			{
+				// Only label when in game!
+				label();
+			}
+		}
+	}
+
+	void label() {
+		// Take screenshot of UI
+		takeScreenshot("", config.targetObject() + " Dataset");
+		// TODO Update ${config.targetObject()}.txt with new image and coordinates of all object boxes
+		updateTxt(config.targetObject());
+		log.info("Took screenshot!");
+	}
+
+	void updateTxt(String object) {
+		// TODO how to get the file name ?
+		log.info("Updating: " + config.targetObject() + ".txt file with object coordinates");
+	}
+
+	void takeScreenshot(String fileName, String subDir)
+	{
+		Consumer<Image> imageCallback = (img) ->
+		{
+			executor.submit(() -> takeScreenshot(fileName, subDir, img));
+		};
+
+		drawManager.requestNextFrameListener(imageCallback);
+	}
+
+	private void takeScreenshot(String fileName, String subDir, Image image)
+	{
+		BufferedImage screenshot = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+		Graphics graphics = screenshot.getGraphics();
+
+		int gameOffsetX = 0;
+		int gameOffsetY = 0;
+
+		graphics.drawImage(image, gameOffsetX, gameOffsetY, null);
+		imageCapture.takeScreenshot(screenshot, fileName, subDir, false, ImageUploadStyle.NEITHER);
+	}
+
+	void rebuild() {
+		log.info("Rebuilding plugin!");
+		// TODO Stop labeling
+		// TODO update KeyEventListener with new key set if it changed
+		// TODO Prompt user to start labelling again
+	}
+
+	private void toggleLabeling()
+	{
+		if (this.labelingActive) {
+			this.labelingActive = false;
+			log.info("Deactivated Labeler!");
+		} else {
+			log.info("Activate Labeler!");
+			this.labelingActive = true;
+		}
 	}
 }
